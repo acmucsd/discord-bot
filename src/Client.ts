69deffec2f75f935e9f2a0ca899cadbd60d068b2
Matei-Alexandru Gardus <matei@gard.us>
2021-03-17 01:06:02 -0700

    Adjust indentation of Client

--- src/Client.ts
+++ src/Client.ts
@@ -50,30 +50,46 @@ export default class Client extends DiscordClient implements BotClient {
       throw new Error('Could not construct Client class: missing bot token in envvars');
     }
     this.settings.token = process.env.BOT_TOKEN;
-        // "!" required here because Discord.js doesn't let you make this mandatory,
-        // so we need the "name" to be an optional assignment.
-        this.settings.presence.activity!.name = process.env.BOT_ACTIVITY;
-        this.settings.maintainerID = process.env.MAINTAINER_USER_ID;
-        this.settings.apiKeys.catAPI = process.env.CAT_API_KEY;
-        this.settings.apiKeys.unsplash = process.env.UNSPLASH_ACCESS_KEY;
+    // "!" required here because Discord.js doesn't let you make this mandatory,
+    // so we need the "name" to be an optional assignment.
+    this.settings.presence.activity!.name = process.env.BOT_ACTIVITY;
+    this.settings.maintainerID = process.env.MAINTAINER_USER_ID;
+    this.settings.apiKeys.catAPI = process.env.CAT_API_KEY;
+    this.settings.apiKeys.unsplash = process.env.UNSPLASH_ACCESS_KEY;
 
-        if (!process.env.ACMURL_USERNAME) {
-          Logger.error('Could not construct Client class: missing ACMURL username in envvars', {
-            eventType: 'initError',
-            error: 'missing ACMURL username in envvars',
-          });
-          throw new Error('Could not construct Client class: missing ACMURL username in envvars');
-        }
-        if (!process.env.ACMURL_PASSWORD) {
-          Logger.error('Could not construct Client class: missing ACMURL password in envvars', {
-            eventType: 'initError',
-            error: 'missing ACMURL password in envvars',
-          });
-          throw new Error('Could not construct Client class: missing ACMURL password in envvars');
-        }
-        this.settings.acmurl.username = process.env.ACMURL_USERNAME;
-        this.settings.acmurl.password = process.env.ACMURL_PASSWORD;
-        this.initialize().then();
+    if (!process.env.ACMURL_USERNAME) {
+      Logger.error('Could not construct Client class: missing ACMURL username in envvars', {
+        eventType: 'initError',
+        error: 'missing ACMURL username in envvars',
+      });
+      throw new Error('Could not construct Client class: missing ACMURL username in envvars');
+    }
+    if (!process.env.ACMURL_PASSWORD) {
+      Logger.error('Could not construct Client class: missing ACMURL password in envvars', {
+        eventType: 'initError',
+        error: 'missing ACMURL password in envvars',
+      });
+      throw new Error('Could not construct Client class: missing ACMURL password in envvars');
+    }
+    if (!process.env.MEMBERSHIP_PORTAL_API_USERNAME) {
+      Logger.error('Could not construct Client class: missing Membership Portal API username in envvars', {
+        eventType: 'initError',
+        error: 'missing Membership Portal API username in envvars',
+      });
+      throw new Error('Could not construct Client class: missing Membership Portal API username in envvars');
+    }
+    if (!process.env.MEMBERSHIP_PORTAL_API_PASSWORD) {
+      Logger.error('Could not construct Client class: missing Membership Portal API password in envvars', {
+        eventType: 'initError',
+        error: 'missing Membership Portal API password in envvars',
+      });
+      throw new Error('Could not construct Client class: missing Membership Portal API password in envvars');
+    }
+    this.settings.acmurl.username = process.env.ACMURL_USERNAME;
+    this.settings.acmurl.password = process.env.ACMURL_PASSWORD;
+    this.settings.portalAPI.username = process.env.MEMBERSHIP_PORTAL_API_USERNAME;
+    this.settings.portalAPI.password = process.env.MEMBERSHIP_PORTAL_API_PASSWORD;
+    this.initialize().then();
   }
 
   /**
