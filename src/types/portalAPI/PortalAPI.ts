b8c84bd485948274f0da4b3215caa9a2995dbb7b
Matei-Alexandru Gardus <matei@gard.us>
2021-03-18 01:38:59 -0700

    Add Checkin Command
    This was interesting to implement!

--- src/types/portalAPI/PortalAPI.ts
+++ src/types/portalAPI/PortalAPI.ts
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import { UUIDv4 } from '../bot/Bot';
 
 /**
@@ -36,4 +37,80 @@ export interface User {
   * provisions and exceptions.
   */
  points: number;
+}
+
+/**
+ * An Event Model from the Membership Portal API.
+ *
+ * @see {@link https://documenter.getpostman.com/view/12949536/TVRedVwQ#00ed6877-79b3-4734-8323-911e1790988c Portal API Documentation}
+ */
+export interface PortalEvent {
+ /**
+  * UUID of Event in database.
+  */
+ uuid: UUIDv4;
+ /**
+  * Organization hosting Event.
+  *
+  * Typically "ACM".
+  */
+ organization: string;
+ /**
+  * Community hosting Event.
+  *
+  * Any one of "General", "AI", "Cyber", etc. is expected here.
+  */
+ community: string;
+ /**
+  * URL to the thumbnail of the Event.
+  */
+ thumbnail: string;
+ /**
+  * URL to the cover photo of the Event.
+  */
+ cover: string;
+ /**
+  * Title of Event.
+  */
+ title: string;
+ /**
+  * Description of Event.
+  *
+  * Typically marketing pitch.
+  */
+ description: string;
+ /**
+  * Location of Event.
+  *
+  * Discord link, Zoom link, lecture hall, etc.
+  */
+ location: string;
+ /**
+  * Link to online Event location.
+  */
+ eventLink: string;
+ /**
+  * Checkin code for Event.
+  */
+ attendanceCode: string;
+ /**
+  * Start time of Event.
+  */
+ start: DateTime;
+ /**
+  * End time of Event.
+  */
+ end: DateTime;
+ /**
+  * Points given to Member if checking in to this Event.
+  */
+ pointValue: string;
+ /**
+  * Whether the event can only be checked into by Staff.
+  */
+ requiresStaff: boolean;
+ /**
+  * Bonus points awarded to any Staff members who check in to an Event.
+  */
+ staffPointBonus: number;
 }
