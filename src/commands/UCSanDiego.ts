feb9b0404b27969986bf2b8bb1236201bb57a5ee
Ronak Shah <rmshah@ucsd.edu>
2021-03-19 22:09:04 -0700

    Update UCSanDiego.ts

--- src/commands/UCSanDiego.ts
+++ src/commands/UCSanDiego.ts
@@ -3,6 +3,14 @@ import { Message } from 'discord.js';
 import Command from '../Command';
 import { BotClient } from '../types';
 
+const copypasta: string[] = [
+  'Using UC San Diego in place of the UCSD acronym better identifies our campus both',
+  'locally and nationally. There’s confusion among San Diego higher education institutions',
+  'because of similar acronyms—UCSD, USD, and SDSU—which we eliminate by using UC San Diego.',
+  'Additionally, this naming convention is consistent with other campuses in the University of',
+  'California system, such as UC Irvine, UC Riverside, UC Santa Barbara, UC Davis, and so on.'
+];
+
 /**
  * Command to correct the spelling of UC San Diego
  */
@@ -19,6 +27,6 @@ export default class UCSanDiegoCommand extends Command {
   }
 
   public async run(message: Message): Promise<void> {
-      await super.respond(message.channel, 'Using UC San Diego in place of the UCSD acronym better identifies our campus both locally and nationally. There’s confusion among San Diego higher education institutions because of similar acronyms—UCSD, USD, and SDSU—which we eliminate by using UC San Diego. Additionally, this naming convention is consistent with other campuses in the University of California system, such as UC Irvine, UC Riverside, UC Santa Barbara, UC Davis, and so on.');
+      await super.respond(message.channel, copypasta.join(' '));
   }
 }
