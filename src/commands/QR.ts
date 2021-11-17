95f6501642851a6906d0f9eef2eccfbadd4b3baa
Matei-Alexandru Gardus <matei@gard.us>
2021-11-17 12:23:46 -0800

    Add QR Command
    For now, the command has NO input validation, so literally anything could be
    encoded into a QR code.

---
+++ src/commands/QR.ts
@@ -0,0 +1,73 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, MessageAttachment, MessageEmbed } from 'discord.js';
+import QRCode from 'easyqrcodejs-nodejs';
+import Command from '../Command';
+import { BotClient } from '../types';
+
+/**
+ * This Command is a simple QR code generator.
+ *
+ * Pass a string, get an ACM-branded QR code publicly shared.
+ */
+export default class QR extends Command {
+  constructor(client: BotClient) {
+    const definition = new SlashCommandBuilder()
+      .setName('qr')
+      .addStringOption((option) => option.setName('content').setDescription('The content to put in the QR code.').setRequired(true))
+      .setDescription('Generates a QR code with the provided text in it. Includes ACM logo!');
+
+    super(client, {
+      name: 'qr',
+      boardRequired: true,
+      enabled: true,
+      description: 'Generates an ACM-branded QR code with a provided text in it.',
+      category: 'Utility',
+      usage: client.settings.prefix.concat('qr <text>'),
+      requiredPermissions: ['SEND_MESSAGES'],
+    }, definition);
+  }
+
+  /**
+   * The workhorse of QR, this just makes a QR code and sends it to the channel.
+   * We don't validate the data of the QR code, though we could.
+   *
+   * @param interaction The Slash Command Interaction instance.
+   */
+  public async run(interaction: CommandInteraction): Promise<void> {
+    // Get the content of the QR code from arguments.
+    const content = interaction.options.getString('content', true);
+
+    // Defer the reply so we can have time to make the QR code.
+    await super.defer(interaction);
+
+    // Make the QR code.
+    //
+    // See Checkin.ts on how QR code arguments work.
+    const qrCode = new QRCode({
+      text: content,
+      colorDark: '#000000',
+      colorLight: '#ffffff',
+      correctLevel: QRCode.CorrectLevel.H,
+      logo: 'src/assets/acm-qr-logo.png',
+      logoBackgroundTransparent: false,
+      quietZone: 30,
+    });
+
+    // Make the Discord attachment for the QR code.
+    const qrCodeDataUrl = await qrCode.toDataURL();
+    const qrCodeBuffer: Buffer = Buffer.from(qrCodeDataUrl.split(',')[1], 'base64');
+    const qrCodeAttachment = new MessageAttachment(qrCodeBuffer, 'qr.png');
+
+    // Add a descriptive embed to make the contents of the QR code more obvious to others.
+    const embed = new MessageEmbed()
+      .setTitle('Done!')
+      .setDescription(`Content of QR code: ${content}`)
+      .setColor('BLUE');
+
+    // Send it!
+    await super.edit(interaction, {
+      embeds: [embed],
+      files: [qrCodeAttachment],
+    });
+  }
+}
