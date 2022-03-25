62a988612a6214168ee9808bf650c7cae229a982
Matei-Alexandru Gardus <matei@gard.us>
2022-03-24 20:10:32 -0700

    Fix Kudos to only allow user mentions
    This avoid mass pings like for @ Member or @ everyone.

--- src/commands/Kudos.ts
+++ src/commands/Kudos.ts
@@ -22,7 +22,7 @@ export default class Kudos extends Command {
   constructor(client: BotClient) {
     const definition = new SlashCommandBuilder()
       .setName('kudos')
-      .addMentionableOption((option) => option.setName('to').setDescription('The user or role to send a note to.').setRequired(true))
+      .addUserOption((option) => option.setName('to').setDescription('The user to send a note to.').setRequired(true))
       .addStringOption((option) => option.setName('for').setDescription('What the note should say.').setRequired(true))
       .setDescription('Sends a thank-you note to someone for you.');
     super(client, {
@@ -39,7 +39,7 @@ export default class Kudos extends Command {
     // Get all the tokens and join them into a message.
     const messageText = interaction.options.getString('for', true);
     // Get the user we're looking to send the note to. We need this to mention them.
-    const mention = interaction.options.getMentionable('to', true);
+    const mention = interaction.options.getUser('to', true);
 
     // This normally won't happen, but we'll anticipate the scenario a channel doesn't exist
     // and account for it in error logs.
@@ -55,6 +55,16 @@ export default class Kudos extends Command {
       });
       await super.respond(interaction, {
         content: `An error was encountered when finding the channel you sent this message in. I know, it's weird. *(Error UUID: ${uuid})`,
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // If the person is sending a Kudos to themselves, we should kinda tell them not to do that.
+    // Seems self-centered.
+    if (mention.id === interaction.member?.user.id) {
+      await super.respond(interaction, {
+        content: 'Sending a Kudos to yourself? I don\'t know, that seems counter-intuitive.',
         ephemeral: true,
       });
       return;
