de39b2187fcb28e2c1cc727632df216b61426ed4
Alex Zhang <acz001@ucsd.edu>
2023-04-02 00:10:54 -0700

    Disable /everyone (#30)
    * Disable /everyone
    
    * Remove auto-formatted code

--- src/commands/Everyone.ts
+++ src/commands/Everyone.ts
@@ -58,7 +58,7 @@ export default class Everyone extends Command {
 
     super(client, {
       name: 'everyone',
-      enabled: true,
+      enabled: false,
       description: 'Ping everyone.',
       category: 'Jokes',
       usage: client.settings.prefix.concat('everyone'),
